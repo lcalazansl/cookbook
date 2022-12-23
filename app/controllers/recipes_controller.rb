2c5bb20ead6ccc7772734b275f0ff1deac825c73
Lucas Luz <lucas@calazansluz.com>
2022-12-23 14:45:03 +0100

    crud refactoring

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,11 @@
 class RecipesController < ActionController::Base
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
     @recipes = Recipe.all
   end
   def show
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
   end
   def new
     @recipe = Recipe.new
@@ -14,20 +16,24 @@ class RecipesController < ActionController::Base
     redirect_to recipes_path(@recipe)
   end
   def edit
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
   end
   def update
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
     @recipe.update(recipe_params)
     redirect_to recipes_path(@recipe)
   end
   def destroy
-    @recipe = Recipe.find(params[:id])
+    # @recipe = Recipe.find(params[:id])
     @recipe.destroy
     redirect_to recipes_path, status: :see_other
   end
 
   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
   def recipe_params
     params.require(:recipe).permit(:name, :instructions, :avg_shelf_life)
